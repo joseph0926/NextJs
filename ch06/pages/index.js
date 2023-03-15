5ce004f2ebfdd1fbf854fa688123c288f62f48f6
김영훈 <joseph0926@naver.com>
2023-03-15 20:41:12 +0900

    SSG_SSR_ex_ch06_2023.03.15

---
+++ ch06/pages/index.js
@@ -0,0 +1,25 @@
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+
+function HomePage(props) {
+  const { events } = props;
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
